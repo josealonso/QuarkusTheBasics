6f5fcd77291bd152ae0dcc9e7da351c575e87a13
josealonso <jose.alonso.programmer@gmail.com>
2024-11-05 17:53:40 +0100

    Add the Controller suffix to a bean.

--- src/main/java/InvoiceBeanController.java
+++ src/main/java/InvoiceBeanController.java
@@ -15,7 +15,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
 
-@Named
+@Named("invoiceBean")
 @ViewScoped
 public class InvoiceBeanController implements Serializable {
 
